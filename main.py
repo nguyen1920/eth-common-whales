ca11e9d821a1ebacae931f689868a4edc956356a
Joshua Trinh Nguyen <joshuatrinhnguyen@Joshuas-MacBook-Pro.local>
2023-11-21 15:26:51 -0500

    working data scraper beta

--- main.py
+++ main.py
@@ -8,11 +8,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import time
 
 def balances(url):
-
     driver = webdriver.Firefox()
     driver.get(url)
+
+    span = driver.find_elements(By.CLASS_NAME, "text-muted")
+    findlastTXN=False
+    i=0
+    while findlastTXN == False:
+        if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
+            findlastTXN = i
+        i=i+1
+
+    findfirstTXN=False
+    while findfirstTXN == False:
+        if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
+            findfirstTXN = i
+        i=i+1
+
+    lastTXN = str(span[findlastTXN].get_attribute('innerHTML'))
+    firstTXN = str(span[findfirstTXN].get_attribute('innerHTML'))
 
     dropdownMenuBalance = driver.find_element(By.ID, "dropdownMenuBalance").get_attribute('innerHTML')
 
@@ -21,15 +38,29 @@ def balances(url):
     except:
         print("Can't load page.")
         pass
+
     high_bal_eth_value = driver.find_element(By.ID, "high_bal_eth_value").get_attribute('innerHTML')
     high_bal_usd_value = driver.find_element(By.ID, "high_bal_usd_value").get_attribute('innerHTML')
     driver.quit()
 
-    return dropdownMenuBalance,high_bal_eth_value,high_bal_usd_value
+    return lastTXN,firstTXN,dropdownMenuBalance,high_bal_eth_value,high_bal_usd_value
 
 def main():
     # URL of the web page
-    url = "https://etherscan.io/address/0xfa31a00a87c766579f0790be73c3763b3e952e94#analytics"
-    print(balances(url))
+    #url = "https://etherscan.io/address/0xfa31a00a87c766579f0790be73c3763b3e952e94#analytics"
+    url_base = "https://etherscan.io/address/"
+
+
+    f = open("demofile2.csv", "a")
+
+    with open("UseMe.csv") as file:
+      for item in file:
+        url = url_base+item.strip()+"#analytics"
+        potential_addresses = balances(url)
+        f.write(url+","+str(potential_addresses)+"\n")
+
+    f.close()
+
+    #time.sleep(3)
 
 main()
