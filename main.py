05d75129df88485872e0b11b789978eff6859d12
Joshua Trinh Nguyen <joshuatrinhnguyen@Joshuas-MacBook-Pro.local>
2023-11-22 15:42:19 -0500

    alpha build

--- main.py
+++ main.py
@@ -18,24 +18,35 @@ def balances(url):
     driver = webdriver.Firefox()
     driver.get(url)
 
-    span = driver.find_elements(By.CLASS_NAME, "text-muted")
-    findlastTXN=False
-    i=0
-    while findlastTXN == False:
-        if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
-            findlastTXN = i
-        i=i+1
+    try:
+        span = driver.find_elements(By.CLASS_NAME, "text-muted")
+    except:
+        span = None
+        lastTXN = None
+        firstTXN = None
 
-    findfirstTXN=False
-    while findfirstTXN == False:
-        if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
-            findfirstTXN = i
-        i=i+1
 
-    lastTXN = str(span[findlastTXN].get_attribute('innerHTML'))
-    firstTXN = str(span[findfirstTXN].get_attribute('innerHTML'))
+    if span:
+        findlastTXN=False
+        i=0
+        while findlastTXN == False:
+            if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
+                findlastTXN = i
+            i=i+1
+        findfirstTXN=False
+        while findfirstTXN == False:
+            if("from" in str(span[i].get_attribute('innerHTML')) and "ago" in str(span[i].get_attribute('innerHTML'))):
+                findfirstTXN = i
+            i=i+1
+        lastTXN = str(span[findlastTXN].get_attribute('innerHTML'))
+        firstTXN = str(span[findfirstTXN].get_attribute('innerHTML'))
 
-    dropdownMenuBalance = driver.find_element(By.ID, "dropdownMenuBalance").get_attribute('innerHTML')
+
+    try:
+        dropdownMenuBalance = driver.find_element(By.ID, "dropdownMenuBalance").get_attribute('innerHTML')
+    except:
+        dropdownMenuBalance = None
+        pass
 
     try:
         element = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"analytics_pageiframe")))
@@ -43,8 +54,17 @@ def balances(url):
         print("Can't load page.")
         pass
 
-    high_bal_eth_value = driver.find_element(By.ID, "high_bal_eth_value").get_attribute('innerHTML')
-    high_bal_usd_value = driver.find_element(By.ID, "high_bal_usd_value").get_attribute('innerHTML')
+    try:
+        high_bal_eth_value = driver.find_element(By.ID, "high_bal_eth_value").get_attribute('innerHTML')
+    except:
+        high_bal_eth_value = None
+        pass
+    try:
+        high_bal_usd_value = driver.find_element(By.ID, "high_bal_usd_value").get_attribute('innerHTML')
+    except:
+        high_bal_usd_value = None
+        pass
+
     driver.quit()
 
     return lastTXN,firstTXN,dropdownMenuBalance,high_bal_eth_value,high_bal_usd_value
@@ -59,12 +79,18 @@ def cleanup(account_details):
     high_bal_eth_value = account_details[3]
     high_bal_usd_value = account_details[4]
 
-    lastTXN = lastTXN.split()  #[0] from, [1] number, [2] days/hrs, [3] number, [4] hrs/mins, [5] ago
-    firstTXN = firstTXN.split()  #[0] from, [1] number, [2] days/hrs, [3] number, [4] hrs/mins, [5] ago
-    dropdownMenuBalance = dropdownMenuBalance.split()
-    dropdownMenuBalance = re.search(r'(?:[\£\$\€]{1}[,\d]+.?\d*)', dropdownMenuBalance[0]).group().replace(",", "").replace("$", "")
-    high_bal_eth_value = high_bal_eth_value.split()[0]
-    high_bal_usd_value = high_bal_usd_value.split()[1].replace(",", "")
+    if lastTXN is not None:
+        lastTXN = lastTXN.split()  #[0] from, [1] number, [2] days/hrs, [3] number, [4] hrs/mins, [5] ago
+    if firstTXN is not None:
+        firstTXN = firstTXN.split()  #[0] from, [1] number, [2] days/hrs, [3] number, [4] hrs/mins, [5] ago
+    if dropdownMenuBalance is not None:
+        dropdownMenuBalance = dropdownMenuBalance.split()
+        dropdownMenuBalance = re.search(r'(?:[\£\$\€]{1}[,\d]+.?\d*)', dropdownMenuBalance[0]).group().replace(",", "").replace("$", "")
+    if high_bal_eth_value is not None:
+        high_bal_eth_value = high_bal_eth_value.split()[0]
+    if high_bal_usd_value is not None:
+        high_bal_usd_value = high_bal_usd_value.split()[1].replace(",", "")
+
 
     return lastTXN,firstTXN,dropdownMenuBalance,high_bal_eth_value,high_bal_usd_value
 
